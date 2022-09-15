ea9631e5ff2d8c2c6e3f939ea1b92ffb702bf62c
hanna1122 <khn951410@naver.com>
2022-09-15 14:07:17 +0900

    gateway test

--- user/src/main/java/com/project/user/controller/CommonController.java
+++ user/src/main/java/com/project/user/controller/CommonController.java
@@ -1,16 +1,21 @@
 package com.project.user.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class CommonController {
+    private final Environment env;
+    @GetMapping("/user-service/healthCheck")
+    public String healthCheck(){
 
-    @GetMapping("/healthCheck")
-    public ResponseEntity healthCheck(){
-        return new ResponseEntity("success", HttpStatus.OK);
+        return String.format("It's working in User Service on PORT %s",
+                env.getProperty("local.server.port"));
     }
 
 }
