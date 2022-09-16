63f4ceda47f876b98fb3f46ba24b481d1d957a99
hanna1122 <khn951410@naver.com>
2022-09-16 13:30:07 +0900

    websecurity 적용

---
+++ user/src/main/java/com/project/user/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.project.user.controller;
+
+import com.project.user.service.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+}
---
+++ user/src/main/java/com/project/user/service/UserService.java
@@ -0,0 +1,12 @@
+package com.project.user.service;
+
+import com.project.user.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+}
