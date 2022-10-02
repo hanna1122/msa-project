7915401b233629d79a4a05be7479ad53580c38a6
hanna1122 <khn951410@naver.com>
2022-10-02 19:30:41 +0900

    swagger 설정 및 JWT 인증 추가

--- gateway/src/main/java/com/project/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/project/gateway/GatewayApplication.java
@@ -3,6 +3,9 @@ package com.project.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +15,8 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
+		return security.csrf().disable().build();
+	}
 }
