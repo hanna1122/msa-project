6fae41d3fd7f1056ab9cac3d5c446f070ed9c230
hanna1122 <khn951410@naver.com>
2022-10-05 18:37:14 +0900

    feign 적용 - Gym service 이용권 등록

---
+++ payment/src/main/java/com/project/payment/service/PaymentService.java
@@ -0,0 +1,41 @@
+package com.project.payment.service;
+
+import com.project.payment.domain.Order;
+import com.project.payment.domain.OrderStatus;
+import com.project.payment.domain.Payment;
+import com.project.payment.dto.OrderDto;
+import com.project.payment.feign.client.GymServiceClient;
+import com.project.payment.feign.dto.LessonResponse;
+import com.project.payment.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+    public final PaymentRepository paymentRepository;
+
+    public final OrderService orderService;
+
+    public final GymServiceClient gymServiceClient;
+
+    public Payment savePayment(OrderDto orderDto, String userId){
+        Order updateOrder = orderService.updateOrder(orderDto.getId(), OrderStatus.FINISHED);
+        OrderDto dto = OrderDto.builder()
+                .id(orderDto.getId())
+                .userId(userId)
+                .lessonId(updateOrder.getLessonId())
+                .lessonName(updateOrder.getLessonName())
+                .lessonPrice(updateOrder.getLessonPrice())
+                .paymentType(updateOrder.getPaymentType())
+                .build();
+        Payment payment = paymentRepository.save(new Payment(dto));
+        gymServiceClient.saveTicket(orderDto, userId);
+        return payment;
+    }
+
+    public Order cancelPayment(OrderDto orderDto, String userId){
+        return orderService.updateOrder(orderDto.getId(), OrderStatus.CANCEL);
+    }
+}
