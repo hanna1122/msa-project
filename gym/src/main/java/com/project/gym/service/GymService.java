c725ab09a86d3f644bdaa2b8f66f532f0f56012c
hanna1122 <khn951410@naver.com>
2022-10-09 20:05:31 +0900

    feign 적용 - userType 변경

--- gym/src/main/java/com/project/gym/service/GymService.java
+++ gym/src/main/java/com/project/gym/service/GymService.java
@@ -3,6 +3,7 @@ package com.project.gym.service;
 import com.project.gym.domain.Ticket;
 import com.project.gym.dto.TicketDto;
 import com.project.gym.feign.client.TrainerServiceClient;
+import com.project.gym.feign.client.UserServiceClient;
 import com.project.gym.feign.dto.LessonResponse;
 import com.project.gym.feign.dto.OrderRequest;
 import com.project.gym.repository.AttendanceRepository;
@@ -18,14 +19,19 @@ public class GymService {
 
     private final TrainerServiceClient trainerServiceClient;
 
+    private final UserServiceClient userServiceClient;
+
     public Ticket saveTicket(OrderRequest orderRequest, String userId){
         LessonResponse lessonResponse = trainerServiceClient.getLesson(orderRequest.getLessonId());
+        Ticket saveTicket;
         if(lessonResponse.getLessonType().equals("GENERAL")){
             TicketDto generalDto = TicketDto.generalTicket(orderRequest, lessonResponse);
-            return ticketRepository.save(Ticket.generalTicket(generalDto));
+            saveTicket = Ticket.generalTicket(generalDto);
         }else{
             TicketDto personalDto = TicketDto.personalTicket(orderRequest, lessonResponse);
-            return ticketRepository.save(Ticket.personalTicket(personalDto));
+            saveTicket = Ticket.personalTicket(personalDto);
         }
+        userServiceClient.updateUserType(userId, saveTicket.getType());
+        return ticketRepository.save(saveTicket);
     }
 }
