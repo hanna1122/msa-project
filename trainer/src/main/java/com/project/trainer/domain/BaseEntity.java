fe3fa032b2d0973101fb5f2a5a8c6f9c9c90c6e8
hanna1122 <khn951410@naver.com>
2022-10-14 16:16:10 +0900

    record 테이블 생성 및 등록 API 추가

---
+++ trainer/src/main/java/com/project/trainer/domain/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.project.trainer.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    private String createdBy;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+}
